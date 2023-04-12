0e09a1f740f1040f8787a0287bee74d89c58764c
Aspire0071400 <prabhujivats@gmail.com>
2023-04-12 21:00:24 +0530

    web-Client active now

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:social_i/responsive/mobile_screen_layout.dart';
 import 'package:social_i/responsive/responsive_layout_screen.dart';
 import 'package:social_i/responsive/web_screen_layout.dart';
 import 'package:social_i/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDC5gkDggx3qX-emuesUyUx7Na4hs8ZHCU",
+          appId: "1:630322317672:web:f0c9c32bbc1ee44c9eeedd",
+          messagingSenderId: "630322317672",
+          projectId: "social-i-421b2",
+          storageBucket: "social-i-421b2.appspot.com"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
